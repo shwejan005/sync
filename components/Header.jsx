5b94070f3130aab4caa1aefd2a6d2046c78a61a2
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-03 16:08:10 +0530

    Clerk auth setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ModeToggle from './ModeToggle'
 import { Separator } from './ui/separator'
+import { SignedOut, SignInButton } from '@clerk/nextjs'
+import { SignedIn,  UserButton } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <div className="relative h-14 bg-secondary text-primary flex justify-center items-center">
+    <div className="relative h-14 bg-secondary text-primary flex justify-center items-center gap-6">
       <Separator className="absolute top-0 left-0 w-full bg-primary/40" />
+
       <ModeToggle />
       <h1>Header</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   )
 }
