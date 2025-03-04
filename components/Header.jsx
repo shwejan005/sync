50cee1993228e43bda5f50e90977a89749af2657
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-04 20:36:37 +0530

    Done with Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,34 +2,49 @@
 
 import React from 'react'
 import ModeToggle from './ModeToggle'
-import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/nextjs'
-import { cinzel } from '@/app/layout'
+import { SignedOut, SignInButton, SignedIn } from '@clerk/nextjs'
 import Link from 'next/link' 
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
+import { Button } from './ui/button'
+import UserMenu from './user-menu'
 
 const Header = () => {
   const { theme } = useTheme()
 
-  // Set correct logo based on theme
   const logoSrc = theme === 'dark' ? '/images/sync-b.png' : '/images/sync-w.png'
 
   return (
-    <header className="container mx-auto flex items-center justify-between">
-      <nav className={`${cinzel.className} text-4xl`}>
+    <header className="container mx-auto">
+
+      <nav className='py-6 px-4 flex justify-between items-center'>
         <Link href='/'>
-          <Image src={logoSrc} alt='logo' width={80} height={80} />
+          <Image src={logoSrc} alt='logo' width={130} height={40} />
         </Link>
+
+        <div className='flex items-center justify-center gap-4'>
+          <ModeToggle />
+
+          <Link href='/project/create'>
+            <Button>
+              Create Project
+            </Button>
+          </Link>
+
+          
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/onboarding'>
+              <Button>
+                Get Started
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
+
       </nav>
-
-      <ModeToggle />
-
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   )
 }
